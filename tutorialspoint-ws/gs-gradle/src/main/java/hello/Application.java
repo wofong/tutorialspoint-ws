95c245c6c174a5737e3ca4919e598f51ba4099ff
wofong <wofong@hotmail.com>
2016-08-17 00:52:22 +0800

    added comment 3

--- tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
+++ tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
@@ -8,6 +8,7 @@ public class Application {
 	public static void main(String[] args){
 		//this is a comment, just to see the git changes....
 		//2
+		//3
 		SpringApplication.run(Application.class, args);
 	}
 }
