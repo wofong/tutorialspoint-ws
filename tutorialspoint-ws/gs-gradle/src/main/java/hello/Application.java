840b98d3fce908a763750537030b9131b0ca27dd
wofong <wofong@hotmail.com>
2016-08-17 00:58:22 +0800

    changed 2 and 3.

--- tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
+++ tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args){
 		//this is a comment, just to see the git changes....
-		//2
-		//3
+		//removed comment 2 and 3
 		SpringApplication.run(Application.class, args);
 	}
 }
