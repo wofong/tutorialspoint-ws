dccf2b03ff6f6f7b60f075e0bfda5f786da43a9d
wofong <wofong@hotmail.com>
2016-08-17 00:48:23 +0800

    modified Application.java for comments

--- tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
+++ tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args){
+		//this is a comment, just to see the git changes.
 		SpringApplication.run(Application.class, args);
 	}
 }
