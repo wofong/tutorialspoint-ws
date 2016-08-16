1d5c9bba36b895e803bbf71400b2f4d4a67a5985
wofong <wofong@hotmail.com>
2016-08-17 00:51:54 +0800

    modified comments

---
+++ tutorialspoint-ws/gs-gradle/src/main/java/hello/Application.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args){
+		//this is a comment, just to see the git changes....
+		//2
+		SpringApplication.run(Application.class, args);
+	}
+}
